a2b8340ff9e1a0ec90c8dadccaf8e3541e551193
Ashwin Sekhari <ashwinsekhari@gmail.com>
2021-02-23 18:13:21 +0530

    license finalise

--- client/src/contracts/deth.sol
+++ client/src/contracts/deth.sol
@@ -6,21 +6,35 @@ import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contr
 
 
 contract Deflix is ERC721, Ownable{
-
     /**
     * @dev Mapping from NFT ID to metadata uri => ideally a link pointing to json stored on IPFS (locally for now).
     */
     mapping (uint256 => string) internal idToUri; //storing metadata
-    uint256 licenseCount = 0;
+    uint256 internal licenseCount = 0; 
     
-    mapping (address => uint256[]) internal owners; //Track of liceenses by address
+    mapping (address => uint256[]) internal owners; //Track of licenses by address
+    uint256 internal streamingRate = 10;
   
     constructor (
     )
     
     ERC721 ( "Deflix_License", "DX")
     {
+    }
     
+    function _deleteLicenseOwner(uint256 licenseId) internal {
+        address ownedBy = ownerOf(licenseId);
+        uint i = 0;
+        //find index
+        while (owners[ownedBy][i] != licenseId) {
+            i++;
+        }
+        //delete element
+        while (i<owners[ownedBy].length-1) {
+            owners[ownedBy][i] = owners[ownedBy][i+1];
+            i++;
+        }
+        delete owners[ownedBy][owners[ownedBy].length-1]; //not sure will work or not | Do some tests
     }
     
     //Owner Only functions
@@ -37,14 +51,22 @@ contract Deflix is ERC721, Ownable{
     }
     
     function destoryLicense(uint256 licenseId) public onlyOwner {
+        require(_exists(licenseId), "DEFLIX: Destroying nonexistent license");
         if (bytes(idToUri[licenseId]).length != 0)
         {
             delete idToUri[licenseId];
         }
-        // TODO: remove an item from owners list if the license is destoryed.
+        _deleteLicenseOwner(licenseId);
         _burn(licenseId);
     }
     
+    function setStreamingRate(uint256 rate) public onlyOwner {
+        streamingRate = rate;
+    }
+    
+    function getStreamingRate() public view returns (uint256 ){
+        return streamingRate;
+    }
     
     //Public Functions
     function getLicenseDetails(uint256 licenseId) public view returns (string memory) {
@@ -56,5 +78,17 @@ contract Deflix is ERC721, Ownable{
         return owners[msg.sender];
     }
     
-    // TODO: Add a function for transfering license (keep in mind about idToUri and owners)
+    function getLicenseOwnerAddress(uint256 licenseId) public view returns (address){
+        require(_exists(licenseId), "DEFLIX: nonexistent license");
+        return ownerOf(licenseId);
+    }
+    
+    function transferLicense(uint256 licenseId, address to) public returns (bool){
+        require(_exists(licenseId), "DEFLIX: nonexistent license");
+        
+        _deleteLicenseOwner(licenseId); //delete old owner
+        owners[to].push(licenseId); //add new owner
+        transferFrom(msg.sender, to, licenseId); //finally transfer owner
+        return true;
+    } 
 }
