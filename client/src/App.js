918d186bb953e786369dd031cc9a2462f2a793da
sudhansu <Sudhansu>
2021-02-26 13:13:33 +0530

    minor non reqd. change

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,5 @@
 import React from 'react';
-import {
-  Route,
-  BrowserRouter as Router,
-  Switch,
-} from "react-router-dom";
+import { Route,BrowserRouter as Router,Switch } from "react-router-dom";
 import Home from './Home';
 import Player from './Player';
 import License from './License';
